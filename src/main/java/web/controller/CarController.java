3e21b91a07e427b78ef393aa61d09dca94e89ce9
Egor Pushka <pushkartegor@gmail.com>
2023-09-24 16:09:25 +0300

    fix : rework code for a mentor's comments

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,7 +4,6 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.services.CarService;
 
@@ -12,7 +11,6 @@ import java.util.Optional;
 
 
 @Controller
-@RequestMapping("/car")
 public class CarController {
 
     private final CarService carService;
@@ -21,7 +19,7 @@ public class CarController {
         this.carService = carService;
     }
 
-    @GetMapping()
+    @GetMapping(value = "/cars")
     public String printCar(@RequestParam(value = "count", required = false)
                            Optional<Integer> count, Model model) {
         model.addAttribute("cars", carService.indexCarsByCount(count.orElse(0)));
